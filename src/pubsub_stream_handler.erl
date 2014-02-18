e9d40574ff92be7f3acef23f01ff426a4b14227e
Ilya Beda <ir4y.ix@gmail.com>
2014-02-18 21:18:51 +0800

    Add dummy redis subscribe example

--- src/pubsub_stream_handler.erl
+++ src/pubsub_stream_handler.erl
@@ -5,10 +5,15 @@
 -export([info/3]).
 -export([terminate/2]).
 
+-record(state, {subscribe}).
 
 init(_Transport, Req, _Opts, _Active) ->
 	io:format("bullet init~n"),
-	{ok, Req, state}.
+    {Channel, _} = cowboy_req:binding(channel, Req),
+    {ok, Subscribe} = eredis_sub:start_link(),
+    ok = eredis_sub:controlling_process(Subscribe, self()),
+    ok = eredis_sub:subscribe(Subscribe, [Channel]),
+	{ok, Req, #state{subscribe=Subscribe}}.
 
 stream(<<"ping: ", Name/binary>>, Req, State) ->
 	io:format("ping ~p received~n", [Name]),
@@ -16,6 +21,14 @@ stream(<<"ping: ", Name/binary>>, Req, State) ->
 stream(_Data, Req, State) ->
 	{ok, Req, State}.
 
+info({subscribed, Channel, _Pid}, Req, State) ->
+    io:format("Subscribed to ~p~n", [Channel]),
+    eredis_sub:ack_message(State#state.subscribe),
+    {ok, Req, State};
+info({message, _Channel, Mes, _Pid}, Req, State) ->
+    io:format("Get message ~p~n", [Mes]),
+    eredis_sub:ack_message(State#state.subscribe),
+    {reply, Mes, Req, State};
 info(Info, Req, State) ->
 	io:format("info received ~p~n", [Info]),
 	{ok, Req, State}.
